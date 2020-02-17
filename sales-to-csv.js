4855b4a529e9d51dde3cd98c2ba1f9b27a842319
EvoAv <darwinrecreant@gmail.com>
2020-02-17 23:34:34 +0000

    minor bug fix

--- sales-to-csv.js
+++ sales-to-csv.js
@@ -3,14 +3,19 @@
   const rows = $$('table tbody tr').map((elem) => {
     return [].slice.call(elem.querySelectorAll('li')).map((el) => el.textContent);
   });
-  const csv = "data:text/csv;charset=utf-8," + [[...headers], ...rows].map((row) => {
-    return row.map((cell) => {
+  headers[4] = "Day"
+  const csv = "data:text/csv;charset=utf-8," + [headers, ...rows].map((row, i) => {
+    const d = (row[2].split(" ")[0].replace(/\//g, "-"));
+    row = row.map((cell) => {
       cell = cell.replace(/"/g, '""').replace(/^(-|\+|@|=|S\$)/g, "");
-      if (cell.search(/[ ",\n]/) > -1) {
+      if (cell.search(/[#",\n]/) > -1) {
         return `"${cell}"`;
       }
       return cell;
-    }).join(',');
+    });
+    if (i)
+      row.push(d);
+    return row.join(',');
   }).join("\n");
   var encodedUri = encodeURI(csv);
   var link = document.createElement("a");
