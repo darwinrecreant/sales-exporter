60565a5dea6e92ce0163401dfc1f352499e09cc8
EvoAv <darwinrecreant@gmail.com>
2020-02-18 00:04:01 +0000

    minor bug fix

--- sales-to-csv.js
+++ sales-to-csv.js
@@ -4,7 +4,7 @@
     return [].slice.call(elem.querySelectorAll('li')).map((el) => el.textContent);
   });
   headers[4] = "Day";
-  const csv = "data:text/csv;charset=utf-8," + [headers, ...rows].map((row, i) => {
+  const csv = [headers, ...rows].map((row, i) => {
     const d = (row[2].split(" ")[0].replace(/\//g, "-"));
     row = row.map((cell) => {
       cell = cell.replace(/"/g, '""').replace(/^(-|\+|@|=|S\$)/g, "");
@@ -17,7 +17,7 @@
       row.push(d);
     return row.join(',');
   }).join("\n");
-  var encodedUri = encodeURIComponent(csv);
+  var encodedUri = "data:text/csv;charset=utf-8,%EF%BB%BF" + encodeURIComponent(csv);
   var link = document.createElement("a");
   link.setAttribute("href", encodedUri);
   link.setAttribute("download", `sales_${new Date().toISOString().split('T')[0]}.csv`);
